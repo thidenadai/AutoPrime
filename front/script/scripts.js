a7b5d3b76c9d183eb9adc12f5945cc2bfb903ddb
thidenadai <tvdenadai@gmail.com>
2024-08-30 07:29:00 -0300

    att

--- front/script/scripts.js
+++ front/script/scripts.js
@@ -42,3 +42,10 @@ document.addEventListener("DOMContentLoaded", () => {
     updateCarousel();
   });
 });
+//script sidebar
+const sidebar = document.querySelector(".sidebar");
+const toggleBnt = document.querySelector(".toggle-btn");
+
+toggleBnt.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
